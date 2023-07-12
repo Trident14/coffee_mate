5447271f7a9034f23433d5fdfcee38c1812ca6bf
Trident14 <sekarinesh14@gmail.com>
2023-07-12 15:03:34 +0530

    Initial Commit

---
+++ CoffeeMate/CoffeeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMateApp.swift
+//  CoffeeMate
+//
+//  Created by Inesh Sekar on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
